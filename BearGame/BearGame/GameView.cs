6104aae3fb0b5c5c0dbe70a0aff88c76370f5cd0
Frank A. Krueger <fak@praeclarum.org>
2012-03-31 11:16:54 -0700

    Game View in

---
+++ BearGame/BearGame/GameView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BearGame
+{
+    class GameView
+    {
+        World _world;
+
+        public GameView(World world, GameSetting settings)
+        {
+            _world = world;
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+        }
+
+        public void Update(GameTime time)
+        {
+        }
+
+        public void Draw()
+        {
+            var vw = 800;
+            var vh = 600;
+            var ws = 400;
+            _world.Draw(new Rectangle((vw-ws)/2, (vh-ws)/2, ws, ws));
+        }
+    }
+}
--- BearGame/BearGame/Villager.cs
+++ BearGame/BearGame/Villager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace BearGame
 {
@@ -30,10 +31,5 @@ namespace BearGame
             this.Health = settings.Bear_HealthDefault;
         }
 
-        public override void Update()
-        {
-            //what to do?
-
-        }
     }
 }
