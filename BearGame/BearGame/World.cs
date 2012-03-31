72cc6b2ed933de59c04c54673f7168568862694b
Frank A. Krueger <fak@praeclarum.org>
2012-03-31 00:35:41 -0700

    Working on world tiles

--- BearGame/BearGame/World.cs
+++ BearGame/BearGame/World.cs
@@ -8,14 +8,20 @@ namespace BearGame
 {
     class World : Layer
     {
+        public const int TileSize = 64;
+
+        Texture2D _tilesTexture;
+
         public void Load(Texture2D tilesTexture, string path)
         {
+            _tilesTexture = tilesTexture;
             LoadTiles(path);
         }
 
         public void Draw(Camera camera)
         {
-
+            var leftPosition = camera.Position.X - (Camera.VisibleSize / 2) * TileSize;
+            var topPosition = camera.Position.Y - (Camera.VisibleSize / 2) * TileSize;
         }
     }
 }
