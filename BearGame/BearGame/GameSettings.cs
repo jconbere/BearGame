fbc262f09ed500a1f3e560fef05b329e2f81eb14
unknown <Takashi@HandofNOD.(none)>
2012-03-31 00:16:23 -0700

    Adding Game Settings / Manager

---
+++ BearGame/BearGame/GameSettings.cs
@@ -0,0 +1,47 @@
+﻿//                string path = "Global_Settings.xml";
+//                XmlSerializer x = new XmlSerializer(GameSettings.GetType());
+//                StreamWriter writer = new StreamWriter(path);
+//                x.Serialize(writer, settings);
+
+//                MySettings settings = new MySettings();
+//                string path = "Global_Settings.xml";
+//                XmlSerializer x = new XmlSerializer(typeof(MySettings));
+//                StreamReader reader = new StreamReader(path);
+//                settings = (TVSettings)x.Deserialize(reader);
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BearGame
+{
+    public class GameSetting
+    {
+        // Map Stuff
+        public int Map_MaxWidth;
+        public int Map_MaxHeight;
+
+        //People Stuff
+        public int Person_HealthMin;
+        public int Person_HealthMax;
+
+        public int Person_HealthPerHug;
+
+        public int Person_HoneyWantedMin;
+        public int Person_HoneyWantedMax;
+
+        public int Person_LoveMin;
+        public int Person_LoveMax;
+
+        // Bear Stuff
+        public int Bear_HealthMax;
+        public int Bear_HealthMin;
+        public int Bear_HealthPerHoney;
+
+        public GameSetting()
+        {
+        }
+    }
+}
---
+++ BearGame/BearGame/SettingsManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+            //GameSetting settings = new GameSetting();
+
+            //if (SettingsManager.Write(settings)) 
+            //{
+            //    //write success
+            //};
+
+            //settings = SettingsManager.Read("Game.xml");
+
+namespace BearGame
+{
+    public class SettingsManager
+    {   
+        public static bool Write(GameSetting settings)
+        {
+            try
+            {
+                XmlSerializer x = new XmlSerializer(settings.GetType());
+                StreamWriter writer = new StreamWriter("Game.xml");
+                x.Serialize(writer, settings);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static GameSetting Read(string path)
+        {
+            GameSetting settings = new GameSetting();
+            try
+            {
+                XmlSerializer x = new XmlSerializer(typeof(GameSetting));
+                StreamReader reader = new StreamReader(path);
+                settings = (GameSetting)x.Deserialize(reader);
+                return settings;
+            }
+            catch
+            {
+                return settings;
+            }
+        }
+    }
+}
