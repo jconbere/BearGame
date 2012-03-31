9411a21e2e77fe8b0bb1d4b3354dd80bed5b6a42
unknown <Takashi@HandofNOD.(none)>
2012-03-31 13:37:04 -0700

    Mine

--- BearGame/BearGame/Villager.cs
+++ BearGame/BearGame/Villager.cs
@@ -18,6 +18,7 @@ namespace BearGame
         const int ActivityThreshold = 7;  // how far from the bear do we update
         const int RespawnThreshold = 10;  //how far fro the bear do we put them back in their starting place
         // ONLY respawn them if both the villager and the respawn location are offscreen
+        public bool IsDead = false;
 
 
         public Villager(GameSetting settings)
@@ -41,32 +42,55 @@ namespace BearGame
             //love =4 neutral
             //love =7 unconditional love
 
+            //movement 0 once a turn, 1 75% 2 50% 3 20%  away
 
             // if no bear closeby.. mill aimlessly
 
-            //if (this.Distance(Myworld.Bear) <= ActivityThreshold)
-            //{ 
-            //    //do on screen stuff
-            //    switch (this.Love)
-            //    {
-            //        case 0:
+            if (Distance(Myworld.Bear,this) <= ActivityThreshold)
+            { 
+                //do on screen stuff
+                int DeltaRow = 0;
+                int DeltaCol = 0;
 
+                DeltaRow = this.c_position.Row -Myworld.Bear.c_position.Row ;
+                DeltaCol = this.c_position.Col - Myworld.Bear.c_position.Col;
+                
+                // simple stupid state machine.  run in the farthest direction
 
-            //            break;
+                if (Math.Abs(DeltaRow) > Math.Abs(DeltaCol))
+                {
+                    if (DeltaRow > 0)
+                    { 
+                            //MoveCell
+                    }
+                    else 
+                    {
+                    
+                    }
+                }
+                else
+                {
+                    if (DeltaCol > 0)
+                    {
+                    }
+                    else
+                    {
+                    }
+                }
+                    //Myworld.IsPassable(this.c_position.Row)
 
-            //        case 1:
-            //            break;
+                FacingDirection = Direction.Down;
 
-            //        default:
-            //            break;
-            //    }
         
-            //}
-            //else if ((this.Distance(bear,this) >= RespawnThreshold) // && ( // calc bear distance))
-            //{ 
-            //    // force respawn
-            //    this.c_position = spawn_position; 
-            //}
+            }
+            else if ((Distance(Myworld.Bear,this) >= RespawnThreshold) && 
+                Math.Abs(Myworld.Bear.c_position.Row - this.spawn_position.Row)>6 &&
+                Math.Abs(Myworld.Bear.c_position.Col - this.spawn_position.Col)>6) // assuming 6 visual radius
+                
+            { 
+                // force respawn
+                if (!IsDead) this.c_position = spawn_position; // Leave bodies alone!
+            }
         }
     }
 }
