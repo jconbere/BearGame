d7dc7a72c4d82aa6ea7ba356b437638eafd9e85b
unknown <Takashi@HandofNOD.(none)>
2012-03-31 11:54:14 -0700

    Enums

---
+++ BearGame/BearGame/enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BearGame
+{
+    struct CellPosition
+    {
+        public int Col;
+        public int Row;
+    }
+    
+    class enums
+    {
+        
+    }
+}
